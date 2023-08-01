a793c8c31d163897effee6d58cc487366629cd13
ayushman-karn <71817720+ayushman-karn@users.noreply.github.com>
2023-08-01 16:39:06 +0530

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,5 +1,6 @@
 const inputArray = [10, 20, 30, 44, 55, 66, 76, 87, 98, 101];
 
+//The below function calculates the sum of given array
 const accumulator = (inputArray) =>
   inputArray.reduce((acc, curr) => acc + curr);
 
